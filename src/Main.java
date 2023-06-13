64d609b0443557ac8d1144ab4263a50f2fb17e05
mileselizabeth <mileseh@mail.uc.edu>
2023-06-13 00:07:19 -0400

    Commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your party affiliation: Democrat [D], Republican [R], Independent [I]?");
+        String partyAffiliation = in.next();
+        if( partyAffiliation.equalsIgnoreCase ("D"))
+        {
+            System.out.println("You get a Democratic Donkey");
+        }
+        else if( partyAffiliation.equalsIgnoreCase("R"))
+        {
+            System.out.println("You get a Republican Elephant");
+        }
+        else if( partyAffiliation.equalsIgnoreCase("I"))
+        {
+           System.out.println("You get an Independent Man");
+        }
+        else
+        {
+            System.out.println("You get Other");
+        }
+
+
+
+    }
+}
